da91f2a985d49e5b1bd16b8f94ebe9ec83e14984
Victor Ozoterio <victorozoterio@gmail.com>
2026-02-05 19:38:30 -0300

    fix(header): renomeia a pasta header-bugfix para header (correção de case sensitivity no Linux)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from './action-confirm-dialog';
 export * from './delete-confirm-dialog';
 export * from './form-error-inline';
-export * from './header-bugfix';
+export * from './header';
 export * from './pagination-footer';
 export * from './table-sortable-header';
