f1d6abdc618a116174f063d0fcd9cf51574f3010
Victor Ozoterio <victorozoterio@gmail.com>
2026-02-05 19:36:35 -0300

    fix(header): renomeia para header-bugfix temporariamente

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from './action-confirm-dialog';
 export * from './delete-confirm-dialog';
 export * from './form-error-inline';
-export * from './header';
+export * from './header-bugfix';
 export * from './pagination-footer';
 export * from './table-sortable-header';
